9c32ac1d170168c6774b2891ccf4a409a84dc955
Emmanuel Bosquet <bjokac@gmail.com>
2020-10-19 22:38:31 +0200

    diantre

--- myscript.js
+++ myscript.js
@@ -1,10 +1,20 @@
 const details = {
-    ecole:
-        "Née en 2019 à Hérouville, en conséquence de la fermeture de l'IMIE à Caen, l'école 404 forme aux métiers du numérique.",
-    chaudronnerie: `La chaudronnerie est l'art de fabriquer toutes sortes de choses à partir de tôle métallique. J'ai deux d'expériences réparties sur plusieurs années, pour avoir alterné entre travail et voygages.
+    ecole: `Née en 2019 à Hérouville, en conséquence de la fermeture de l'IMIE de Caen, l'école 404 forme aux métiers du numérique : 
+- Administrateur Système et réseaux
+- Designer web
+- Développeur Concepteur d'Applications
+- Ingénieur informatique SI et Business Intelligence
+Toutes les formations sont en alternance. Voir ecole404.fr`,
+    chaudronnerie: `La chaudronnerie est l'art de fabriquer toutes sortes de choses à partir de tôle métallique. J'ai deux ans d'expérience répartie sur plusieurs années, pour avoir alterné entre travail et voyages.
 2005 - 2007 : BTS au Lycée Alexis de Tocqueville (Cherbourg) Stage à l'atelier coque des chantiers navals (sous-marins nucléaires)
 2007 : Colas Rail, maintenance de machines d'entretien ferroviaires, aux Mureaux (Mantes-la-jolie)
 2008 : Freudenberg Produktionservice KG, Weinheim (Allemagne) Fabrication de cabines anti-bruits.
 2010 : Howiller (Strasbourg), Constructions métalliques.
 2011 : STX Saint-Nazaire (chantiers de l'atlantique), fabrication d'éléments de construction de la coque des paquebots.`,
+    mqtt:
+        "Le protocole MQTT sert au dialogue entre les machines. Il a été inventé en 1999 pour monitorer l'état d'oléoducs. On s'en sert aujourd'hui pour l'internet des objets (IoT).",
+    tri: `Un algorithme de tri est une suite d'opérations qui remet dans l'ordre une liste d'éléments, par exemple :
+[10, 2, 3, 5, 9, 1, 4, 7, 6, 8, 0]
+Les solutions à ce problème sont nombreuses, chacune avec ses avantages et inconvénients. Le défi est d'optimiser le nombre d'opérations de comparaison et d'intervertissement.
+Cet exercice a un intérêt pédagogique pour le développeur, il oblige à synthétiser sa pensée et à traduire en language informatique des abstractions mathématiques.`,
 }
