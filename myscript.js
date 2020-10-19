f4e67ee275aaf49b2164c610c88b73b54aee4192
Emmanuel Bosquet <bjokac@gmail.com>
2020-10-19 14:42:34 +0200

    first working draft

---
+++ myscript.js
@@ -0,0 +1,10 @@
+const details = {
+    ecole:
+        "Née en 2019 à Hérouville, en conséquence de la fermeture de l'IMIE à Caen, l'école 404 forme aux métiers du numérique.",
+    chaudronnerie: `La chaudronnerie est l'art de fabriquer toutes sortes de choses à partir de tôle métallique. J'ai deux d'expériences réparties sur plusieurs années, pour avoir alterné entre travail et voygages.
+2005 - 2007 : BTS au Lycée Alexis de Tocqueville (Cherbourg) Stage à l'atelier coque des chantiers navals (sous-marins nucléaires)
+2007 : Colas Rail, maintenance de machines d'entretien ferroviaires, aux Mureaux (Mantes-la-jolie)
+2008 : Freudenberg Produktionservice KG, Weinheim (Allemagne) Fabrication de cabines anti-bruits.
+2010 : Howiller (Strasbourg), Constructions métalliques.
+2011 : STX Saint-Nazaire (chantiers de l'atlantique), fabrication d'éléments de construction de la coque des paquebots.`,
+}
